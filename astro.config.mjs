152cf9b103adb067005be640ded05164091c8d69
a_sinaeepour <a_sinaeepour@ASINAEEPOURVM>
2025-02-04 13:48:57 +0330

    allow request to local url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
+import { loadEnv } from "vite";
 
+
+const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
 // https://astro.build/config
 export default defineConfig({
